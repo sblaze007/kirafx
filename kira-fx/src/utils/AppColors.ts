9383623a74c866674c6b62fb439a8b26d27c2941
satvjain_deloitte <satvjain@deloitte.com>
2025-09-10 19:40:06 +0530

    added utils for font size,style,color and theme

---
+++ kira-fx/src/utils/AppColors.ts
@@ -0,0 +1,122 @@
+export const Colors = {
+  // Primary colors
+  primary: "#3324D4",
+  primary12: "#0F0A41",
+  primary11: "#160F5F",
+  primary10: "#1E157C",
+  primary9: "#251A9A",
+  primary8: "#2C1FB7",
+  primary7: "#3324D4",
+  primary6: "#635FD9",
+  primary5: "#9591FF",
+  primary4: "#B0ACFF",
+  primary3: "#D4D0FF",
+  primary2: "#F1EDFF",
+  primary1: "#F8F6FF",
+  primary13: "#DCDAF3",
+  primary14: "#CAC8FC",
+
+  // Secondary colors
+  secondary: "#2F2B43",
+
+  // Neutral and accent colors
+  neutral: "#1A1825",
+  neutral7: "#2F2B43",
+  neutral6: "#605C74",
+  neutral5: "#ACADC1",
+  neutral4: "#D6D6E1",
+  neutral3: "#F0F0F5",
+  neutral2: "#F5F5F7",
+  neutral1: "#FFFFFF",
+  neutral8: "#EAECED",
+  neutral9: "#FEF7FFBF",
+  neutral10: "#D9D9D9",
+
+  // Suceess colors
+  success: "#2E7D32",
+  success4: "#6DA470",
+  success3: "#96BE98",
+  success2: "#D5E5D6",
+  success1: "#EDF7ED",
+
+  // Warning colors
+  warning: "#EF6C00",
+  warning4: "#F4984D",
+  warning3: "#F7B580",
+  warning2: "#FBDABF",
+  warning1: "#FEF5DF",
+  warning5: "#FFF4E5",
+
+  // Error colors
+  error: "#D32F2F",
+  error4: "#E06D6D",
+  error3: "#EDACAC",
+  error2: "#F6D5D5",
+  error1: "#FCDAE2",
+  error5: "#FDEDED",
+
+  turquoise: "#00BDA3",
+
+  // Background and text colors
+  visualiserBgColor: "#E8EBF2",
+  black: "#000000",
+  white: "#ffffff",
+  inputBg: "#FAFAFA",
+  inputBorder: "#E3E3EA",
+  lightGrey: "#dbdbda",
+  grey: "#5F6B7A",
+  green: "#037F0C",
+  themeRed: "#C20029",
+  breadcrumbsBlue: "#0071de",
+  tagColor: "#232323",
+  tagBg: "#DEF5F0",
+  border: "#D0D0D0",
+  backButtonColor: "#040505",
+  highlightSelectedItem: "#D0D0D0",
+  searchText: "#605C74",
+  underlineColor: "#ADA7EE",
+  selectedMeshColor: "#FF0000",
+};
+
+// Text colors
+export const TextColors = {
+  main: Colors.neutral,
+  sub: Colors.neutral6,
+  disable: Colors.neutral4,
+  secondary: Colors.neutral7,
+};
+
+// Chip colors
+export const ChipColors = {
+  healthy: Colors.success2,
+  default: Colors.neutral4,
+  error: Colors.error3,
+  warning: Colors.warning2,
+};
+
+// Card colors
+export const CardColors = {
+  outline: Colors.neutral4,
+};
+
+// Tile colors
+export const TileColors = {
+  imageBg: Colors.neutral2,
+  eyeIcon: Colors.primary,
+  checkBg: Colors.white,
+  check: Colors.neutral5,
+};
+
+export const SectionCardColors = {
+  draftBackground: "#FFF3E0",
+  draftBorder: "#FFE0B2",
+  draftText: "#E65100",
+
+  acceptedBackground: "#F1F8E9",
+  acceptedBorder: "#C5E1A5",
+  acceptedText: "#33691E",
+
+  rejectedBackground: "#FEEBEE",
+  rejectedBorder: "#FECDD2",
+  rejectedText: "#B71C1C",
+};
